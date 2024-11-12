024c45d9687b0d9f5d90b43c06bf552205f57d68
katherine chadwick <kchadwi6@students.kennesaw.edu>
2024-11-12 13:05:43 -0500

    Update and rename AnatomyTrivia.py to TriageTriumph.py

--- Anatomy_Game/Trivia.py
+++ Anatomy_Game/Trivia.py
@@ -1,20 +1,21 @@
-### HUMAN ANATOMY QUIZ GAME ###
+### TRIAGE QUIZ GAME ###
 
-## QUESTIONS (this is the list of questions separated by category (i.e. body systems)
+## QUESTIONS (this is the list of questions separated by category (colors, long answer, etc.)
 # questions = [question1, question2, question3, question4, question5, question6, question7, question8]
 
 questions = [
-    ## 1 - CIRCULATORY SYSTEM QUESTIONS ##
-    [ "What makes up the cardiovascular component of the circulatory system?\n" ,
-    "True or False: the aorta carries oxygenated blood from the heart to the organs and tissues.\n" ,
+    ## 1 - TRIAGE CATEGORY QUESTIONS ##
+    [ "An unresponsive male patient has snoring respirations. His breathing improves when you open his airway. What triage category would this patient be considered?\n" ,
+    "A 66yo male patient is sitting on the ground. His eyes are open, but he cannot answer or follow directions. What triage category would this patient be considered?\n" ,
     "In addition to blood, what else does the circulatory system move throughout the body?\n" ,
-    "True or False: an average adult has 7-8 quarts of blood.\n" ,
-    "Which specialists are certified to diagnose, treat, and prevent diseases of the heart, arteries, and veins?\n" ,
-    "True or False: oxygen-deprived blood enters into the left ventricle of the heart.\n" ,
-    "What carries oxygen-rich blood from the heart throughout the body?\n" ,
-    "True or False: the superior vena cava, the aorta, and the pulmonary artery are all a part of the heart.\n" ,
-    "Blood is made up of plasma, red blood cells, white blood cells, and what else?\n" ,
-    "True or False: hypertension is one of the most common diseases of the circulatory system.\n"],
+    "A 50yo man has bilateral fractured femurs. He has a faint radial pulse and a RR of 24. What triage category would this patient be considered?\n" ,
+    "A 16yo female who is ambulatory (walking) and says she is O.K. What triage category would this patient be considered?\n" ,
+    "A 19yo male with 2nd and 3rd degree burns over 80% of his body. Respirations 24 and pulse about 120. What triage category would this patient be considered?\n" ,
+    "A 42yo woman with no obvious injuries and without a carotid pulse. What triage category would this patient be considered?\n" ,
+    "A 57yo female has a deformed tibia and fibula. She is oriented with respirations of 20 and a pulse of about 100. What triage category would this patient be considered?\n" ,
+    "A 14yo male with a broken arm walking around the scene. What triage category would this patient be considered?\n" ,
+    "A responsive 34yo female has pale, moist skin and respirations of 32/minute. What triage category would this patient be considered?\n"
+    "A 34yo man is unresponsive with brain matter showing. What triage category would this patient be considered?\n"],
     
     ## 2 - DIGESTIVE SYSTEM QUESTIONS ##
     ["Where does the digestive system begin?\n" ,
