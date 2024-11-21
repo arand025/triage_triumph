a8e23866165df53c2a3d06c02c566cbf7df5fd68
arand025 <arandha2@students.kennesaw.edu>
2024-11-21 18:14:18 -0500

    Update trivia.py
    added questions to section 4

--- Anatomy_Game/Trivia.py
+++ Anatomy_Game/Trivia.py
@@ -32,9 +32,22 @@ questions = [
     "What is the main goal of triage?\n"],
     
     ## 4 - DELEGATION QUESTIONS ##
-    ["Which is true about nursing delegation?\n", 
-    "The nurse taught a patient's caregiver how to administer Lovenox(enoxaparin sodium)injections to a patient. How should the RN evaluate if the teaching was effective prior to discharge?\n"
-    ]
+    ["Which is true about nursing delegation?\n A.the nurse can transfer accountability\n B.the nurse is no longer responsible\n C.the nurse can assume all UAPs are qualified\n D.the nurse retains accountability\n", 
+    "The nurse taught a patient's caregiver how to administer Lovenox(enoxaparin sodium)injections to a patient. How should the RN evaluate if the teaching was effective prior to discharge?\n A. arrange for the follow up with home care nurse\n B. ask the caregiver to 'demeonstrate back' her ability to administer it\n C. moitioring patients lab work before discharge\n D.asking the caregiver what she found helpful about the teaching\n", 
+     "What entity outlined the principles of delegation for registered nurses?\n",
+     "In the process of delegation, the nurse understands that the primary person who maintains accountability for the accuracy, safety, and completion of the task delegated remains with whom? A.The delegator of the task\n B.The delegatee of the task\n C.Those who assisted with task completion\n D.The person who documented task completion\n",
+     "Which health care team member is responsible for the coordination and assignments of client care?\n A.charge nurse\n B.nurse manager\n C.cheif nursing officer\n D.unlicensed assistive personnel\n ", 
+    "What is the primary purpose of delegation in nursing practice?\n A.To transfer full responsibility of patient care to another individual\n B.To allow the nurse to focus solely on administrative tasks\n C.To enable the nurse to assign specific tasks to others while retaining accountability\n D.To eliminate the need for communication among healthcare team members\n",
+    "After a surgical procedure, which assessment finding should the nurse prioritize obtaining directly from the client to ensure accurate care planning?\n A.Level of consciousness\n B.Post-operative incision appearance\n C.Nausea and vomiting\n D.Current medication regimen\n", 
+    "A nurse delegator assigns work to a delegatee who has the ability and willingness to do the work but the relationship between the delegator and delegatee is relatively new. How is the delegator's behavior described according to Hersey's Model?\n A.Selling\n B.Telling\n C.Delegating\n D.Participating\n",
+    "The nurse is having an exceptionally busy shift on an obstetrical unit. Which task is the nurse justified in delegating to an unlicensed care provider?\n A.Assessing the size and quantity of clots that are in a client's bedpan and informing the nurse\n B.Giving an anti-inflammatory medication to a client who is eight hours postdelivery\n C.Emptying a client's Foley catheter bag and reporting the volume to the nurse\n D.Helping a first-time mother achieve a good latch when breastfeeding the infant\n"],
+
+
+
+
+
+
+
 
 ## ANSWERS ##
 # VIP: if you make changes in the question zone, don't forget to synchronise this zone (the variable and the list)!
