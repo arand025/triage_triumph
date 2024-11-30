af1fb4fb2d8f7404e7049b497372e5b2c697071b
KCC <kchadwi6@students.kennesaw.edu>
2024-11-29 21:58:52 -0500

    Create DelegationQuestions.py
    I added a py file containing the questions and answers for delegation questions

---
+++ Anatomy_Game/DelegationQuestions.py
@@ -0,0 +1,23 @@
+question1 = ("Which is true about nursing delegation?\n A.the nurse can transfer accountability\n B.the nurse is no longer responsible\n C.the nurse can assume all UAPs are qualified\n D.the nurse retains accountability\n")
+question2 = ("The nurse taught a patient's caregiver how to administer Lovenox(enoxaparin sodium)injections to a patient. How should the RN evaluate if the teaching was effective prior to discharge?\n A. arrange for the follow up with home care nurse\n B. ask the caregiver to 'demeonstrate back' her ability to administer it\n C. moitioring patients lab work before discharge\n D.asking the caregiver what she found helpful about the teaching\n")
+question3 = ("With the delegated task, who is accountable?\n A.Delegator\n B.Delegator\n C.Nurse manager\n D.None\n")
+question4 = ("In the process of delegation, the nurse understands that the primary person who maintains accountability for the accuracy, safety, and completion of the task delegated remains with whom? A.The delegator of the task\n B.The delegatee of the task\n C.Those who assisted with task completion\n D.The person who documented task completion\n")
+question5 = ("Which health care team member is responsible for the coordination and assignments of client care?\n A.charge nurse\n B.nurse manager\n C.cheif nursing officer\n D.unlicensed assistive personnel\n")
+question6 = ("What is the primary purpose of delegation in nursing practice?\n A.To transfer full responsibility of patient care to another individual\n B.To allow the nurse to focus solely on administrative tasks\n C.To enable the nurse to assign specific tasks to others while retaining accountability\n D.To eliminate the need for communication among healthcare team members\n")
+question7 = ("After a surgical procedure, which assessment finding should the nurse prioritize obtaining directly from the client to ensure accurate care planning?\n A.Level of consciousness\n B.Post-operative incision appearance\n C.Nausea and vomiting\n D.Current medication regimen\n")
+question8 = ("A nurse delegator assigns work to a delegatee who has the ability and willingness to do the work but the relationship between the delegator and delegatee is relatively new. How is the delegator's behavior described according to Hersey's Model?\n A.Selling\n B.Telling\n C.Delegating\n D.Participating\n")
+question9 = ("The nurse is having an exceptionally busy shift on an obstetrical unit. Which task is the nurse justified in delegating to an unlicensed care provider?\n A.Assessing the size and quantity of clots that are in a client's bedpan and informing the nurse\n B.Giving an anti-inflammatory medication to a client who is eight hours postdelivery\n C.Emptying a client's Foley catheter bag and reporting the volume to the nurse\n D.Helping a first-time mother achieve a good latch when breastfeeding the infant\n")
+question10 = ("A nurse and an unlicensed assistive personnel (UAP) are caring for clients in a labor and birth unit. Which task should the nurse assign to the UAP?\n A.preform a funddal check on a 2-day postpartum client.\n B.Remove a fetal monitor and assist patient to bathroom.\n C.Teach a new mother how to bottle feed her infant.\n")
+
+
+
+answer1 = ("D")
+answer2 = ("B")
+answer3 = ("A")
+answer4 = ("A")
+answer5 = ("A")
+answer6 = ("C")
+answer7 = ("C")
+answer8 = ("D")
+answer9 = ("C")
+answer10 = ("B")
