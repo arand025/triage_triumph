aa1a34d32b06926a3ef7e184ea7c546a285f2f13
arand025 <arandha2@students.kennesaw.edu>
2024-12-04 20:25:15 -0500

    Update _random.py- update and edit

--- Anatomy_Game/_random.py
+++ Anatomy_Game/_random.py
@@ -1,29 +1,25 @@
-import string
+## random
 
-import numpy as np
-
-from pandas._typing import NpDtype
-
-RANDS_CHARS = np.array(list(string.ascii_letters + string.digits), dtype=(np.str_, 1))
-
-
-def rands_array(nchars, size, dtype: NpDtype = "O", replace: bool = True) -> np.ndarray:
-    """
-    Generate an array of byte strings.
-    """
-    retval = (
-        np.random.choice(RANDS_CHARS, size=nchars * np.prod(size), replace=replace)
-        .view((np.str_, nchars))
-        .reshape(size)
-    )
-    return retval.astype(dtype)
+question1 = ("The nurse knows that common mechanisms of injury involved in penetrating trauma include all of the following except which of the following?\n A.Stabbings\n B.Gunshot wounds\n C.Motor vehicle collisions\n D.Impalements\n")
+question2 = ("True or False: psycological capabilities is a large challange for triage nurses?\n")
+question3 = ("Which of the following are ethcical concerns for Triaging?(type all that apply)\n Resource Allication\n Undertriage\n Discrimination\n All of the above\n")
+question4 = ("True or False: Human resource managements and structural preformance create a large amount of challanges for triaging?\n")
+question5 = ("Is it more common for patients to be triaged as under severe or over severe?\n")
+question6 = ("What is the Acronym for the exam nurses must take?\n")
+question7 = ("What scale are patients asked to rate their pain on?\n")
+question8 = ("Type out the five stages of triaging.(place a common between each level)\n")
 
 
-def rands(nchars) -> str:
-    """
-    Generate one random byte string.
+#answers
+answer1 = ("Gunshot wounds")
+answer2 = ("True")
+answer3 = ("All of the above")
+answer4 = ("True")
+answer5 = ("Under severe")
+answer6 = ("NCELX-RN")
+answer7 = ("1 to ten")
+answer8 = ("Resuscitation,Emergency,Urgent,Semi-Urgent,Non-urgent")
 
-    See `rands_array` if you want to create an array of random strings.
 
-    """
-    return "".join(np.random.choice(RANDS_CHARS, nchars))
+
+
