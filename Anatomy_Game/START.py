0045715b8e7f77dba16b9728c392fa905fadfc56
arand025 <arandha2@students.kennesaw.edu>
2024-12-04 20:22:41 -0500

    Update START.py-ashleen remove commas

--- Anatomy_Game/START.py
+++ Anatomy_Game/START.py
@@ -1,13 +1,13 @@
 
 #START questions 
 
-question1 = ("What does START stand for?\n"),
-question2 = ("What Triage System does the United States use Emergency Department?\n"),
+question1 = ("What does START stand for?\n")
+question2 = ("What Triage System does the United States use Emergency Department?\n")
 question3 = ("Which one of these parameters is not one of the three physiological parameters that are assessed according to START triage?\n A.Apperence\n B.Pulse\n C.Respiration\n D.Mental Status\n"),
-question4 = ("How long should START triage take per patient?\n"),
-question5 = ("What is STEP TWO of START triage?\n"),
-question6 = ("What priority is a patient that's not breathing, even after attempting to open airway?\n"),
-question7 = ("What are the characteristics of a priority 0 (black tag) patient?\n"),
+question4 = ("How long should START triage take per patient?\n")
+question5 = ("What is STEP TWO of START triage?\n")
+question6 = ("What priority is a patient that's not breathing, even after attempting to open airway?\n")
+question7 = ("What are the characteristics of a priority 0 (black tag) patient?\n")
 
 
 #answers
