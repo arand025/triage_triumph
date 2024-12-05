6998ee849b237da877e49caff52961dd9cbb83c2
arand025 <arandha2@students.kennesaw.edu>
2024-12-05 14:45:14 -0500

    Update GeneralQuestions.py

--- Anatomy_Game/GeneralQuestions.py
+++ Anatomy_Game/GeneralQuestions.py
@@ -1,18 +1,19 @@
 ## general Questions 
 
-question1 = ("Which of the following patients would be considered a delayed patient?\n")
-question2 = ("Which of the following is true about START triage?\n")
-question3 = ("Which of the following patients would be considered immediate?\n")
-question4 = ("Which of the following patients would be considered immediate?.\n" )
-question5 = ("Which of the following patients would be considered immediate?\n")
-question6 = ("Which of the following patients would be considered minor?\n")
-question7 = ("What is the main goal of triage?\n")
+question1 = ("Which of the following patients would be considered a delayed patient?\n A.a patient with a broken finger\n B.a patient who has an uncontrolled arterial bleed and is pale\n C.patient who is laying supine complaining they cannot feel or move extremities\n D.a patient who is pulseless and apneic\n")
+question2 = ("Which of the following is true about START triage?\n A.it utilizes three basic steps for brief assessment\n B.it was developed in the military\n C.is only performed on decreased patients\n D.can only be done in a hospital lobby\n ")
+question3 = ("Which of the following patients would be considered immediate?\n A.a patient with burns to the whole lower half of their body\n B.16-year-old with a Femur fracture\n C.52-year-old with heart burn and a minor abrasion to the left elbow\n D.patient with an open pneumothorax\n")
+question4 = ("Which of the following patients would be considered immediate?.\n A.a patient who is siting on the curb with a broken ankle and cannot walk\n B.a patient with a minor laceration to the left thigh\n C.a patient who has a femur fracture\n D.52-year-old patient who is running and screaming 'we are going to die'\n")
+question5 = ("Which of the following patients would be considered immediate?\n A.28 yo with a back injury\n B.bystander w/ circumferential burn to forearm\n C.74 yo w/ both legs amputated\n D.first responder w/ femur fracture\n")
+question6 = ("Which of the following patients would be considered minor?\n A. 44 yo patient who is cool, pale, diaphoretic\n B.23 yo who has an abrasion on the whole posterior side of left leg\n C.patient who is unconscious and breathing 40 times a min\n D.32 yo w/ bilateral femur fractures\n F.23 yo who has an abrasion on the whole posterior side of left leg\n")
+question7 = ( "What is the main goal of triage?\n A.to assess each patient thoroughly and transport to the closest facility\n B.to transport all immediate patients as soon as the hospitals are open\n C.to be rapid and brief w/ the assessment in order to spread patients to treatment units\n D.to be rapid in the transportation and treatment of the expectant patients\n")
 
 
-answer1 = ("Patient who is laying supine complaining they cannot feel or move extremities\n")
-answer2 = ("It utilizes basic steps for brief assessment\n")
-answer3 = ("A patient with an open pneumothorax\n")
-answer4 = ("A 52yo patient who is running around screaming 'We are going to die\n") 
-answer5 = ("First responder with femur fracture\n")
-answer6 = ("23yo who has an abrasion on the whole posterior side of left leg\n") 
-answer7 = ("To be rapid and brief with the assessment in order to spread patients to treatment units\n")
+
+answer1 = ("c")
+answer2 = ("a")
+answer3 = ("d")
+answer4 = ("d") 
+answer5 = ("d")
+answer6 = ("b") 
+answer7 = ("c")
