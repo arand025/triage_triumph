5fc1ecec54450c1d962e693281e42093eec740f4
arand025 <arandha2@students.kennesaw.edu>
2024-12-04 20:01:09 -0500

    added start.py- ashleen

---
+++ Anatomy_Game/START.py
@@ -0,0 +1,20 @@
+
+#START questions 
+
+question1 = ("What does START stand for?\n"),
+question2 = ("What Triage System does the United States use Emergency Department?\n"),
+question3 = ("Which one of these parameters is not one of the three physiological parameters that are assessed according to START triage?\n A.Apperence\n B.Pulse\n C.Respiration\n D.Mental Status\n"),
+question4 = ("How long should START triage take per patient?\n"),
+question5 = ("What is STEP TWO of START triage?\n"),
+question6 = ("What priority is a patient that's not breathing, even after attempting to open airway?\n"),
+question7 = ("What are the characteristics of a priority 0 (black tag) patient?\n"),
+
+
+#answers
+answer1 = ("Simple triage and rapid treatment")
+answer2 = ("START")
+answer3 = ("A")
+answer4 = ("30 seconds")
+answer5 = ("Assesing RPM")
+answer6 = ("Priorty 0")
+answer7 = ("Not breathing")
